3ce949228ba878cec30e61e84f24942513298523
nguy0907 <nguy0907@algonquinlive.com>
2021-12-06 12:01:48 -0500

    adding js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const $container = document.getElementById('container')
+const $reveal = document.getElementById('reveal')
+const $hamburgerMenu = document.getElementById('hamburgerMenu')
+
+
+
+$reveal.addEventListener('click', function(e){
+
+if ($hamburgerMenu){
+    $hamburgerMenu.classList.toggle('click')
+    
+}
+})
