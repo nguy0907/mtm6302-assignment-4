0510fbc7556a423141c009973e16332688b6bcd8
nguy0907 <nguy0907@algonquinlive.com>
2021-12-08 17:40:27 -0500

    adding js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const $container = document.getElementById('container')
 const $reveal = document.getElementById('reveal')
 const $hamburgerMenu = document.getElementById('hamburgerMenu')
-
+const $scroll = document.getElementById('scroll')
+const $paragraph2 = document.getElementById('paragraph2')
+const $link2 = document.getElementById('link2')
 
 
 $reveal.addEventListener('click', function(e){
@@ -10,4 +12,14 @@ if ($hamburgerMenu){
     $hamburgerMenu.classList.toggle('click')
     
 }
+})
+
+$link2.addEventListener('click', function(){
+    // const $scrolldown = e.target.closest('scrolldown')
+  
+    if($paragraph2){ 
+  $paragraph2.classList.toggle('scroll')
+     }
+    
+  
 })
